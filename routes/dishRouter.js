698405629e569c92641e034a568690c1196f1af5
P-Sankethreddy <Bobmarley@12345>
2019-12-22 13:40:28 +0530

    All Router files are added to the Exp-Gen succesfully &Run

---
+++ routes/dishRouter.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const dishRouter = express.Router();
+
+dishRouter.use(bodyParser.json());
+// const dishRouterId = express.Router();
+
+// dishRouterId.use(bodyParser.json());
+
+dishRouter.route('/')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send all the dishes to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /dishes');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all dishes');
+});
+dishRouter.route('/:dishId').all((req, res, next) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	next();
+}).get((req, res, next) => {
+	res.end('Will send the dish to you! ' + req.params.dishId);
+}).post((req, res, next) => {
+	res.statusCode = 403;
+	res.end('POST operation is not supported');
+}).put((req, res, next) => {
+	res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
+	
+}).delete((req, res, next) => {
+	res.end('Deleting the dish ' + req.params.dishId);
+});
+
+module.exports = dishRouter;
+// module.exports = dishRouterId;
