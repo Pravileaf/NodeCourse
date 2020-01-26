8279716b8317075bc11295225cb6f4cdd1b4141b
P-Sankethreddy <51527304+P-Sankethreddy@users.noreply.github.com>
2020-01-26 19:47:55 +0530

    facebook-commit

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports={
 'secretKey':'12345-67890-09876-54321',
-'mongoUrl':'mongodb://localhost:27017/EXP-GEN'
+'mongoUrl':'mongodb://localhost:27017/EXP-GEN',
+'facebook': {
+    clientId: '189741868773525',
+    clientSecret: 'a0c897221e274c100f9069572f841461'
+}
 }
